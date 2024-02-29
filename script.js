fb3d5a505e7c4de1a39384a0f2f4e8ef9980bb77
KhashayarS <khashayar.sabzevarizadeh@gmail.com>
2024-02-29 18:06:20 +0330

    Add and test the getUserChoice funciton

--- script.js
+++ script.js
@@ -9,9 +9,27 @@ function getComputerChoice() {
 	return randomItem
 }
 
-// Test getComputerChoice
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+function getUserChoice() {
+	/*
+	 * This function gets the user choice via prompt
+	 */
+
+	let userChoice = prompt("Select one of items below and enter the complete name (it's case-insensitive):\n\nrock\npaper\nseasors");
+
+	// make it case-insensitive
+	userChoice = userChoice.toLowerCase();
+
+	// check if the user choice is valid
+	const validItems = ['rock', 'paper', 'seasors'];
+	while (!validItems.includes(userChoice)) {
+		userChoice = prompt("Sorry, the item you selected is not valid! You should type one of these exact words; the case does not matter, but write the complete word!\n\nrock\npaper\nseasors")
+		userChoice = userChoice.toLowerCase();
+	}
+
+	return userChoice
+}
+
+// test getUserChoice function
+console.log(getUserChoice());
 
 
