736c8b28b426fafba9fed8d20dab0ba0c21b6253
KhashayarS <khashayar.sabzevarizadeh@gmail.com>
2024-02-29 17:48:28 +0330

    Add and test the getComputerChoice function to the script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+function getComputerChoice() {
+	/*
+	 * This function returns a random choice for the computer
+	 */
+
+	const choicesList = ['rock', 'paper', 'seasors']
+	let randomIndex = Math.floor(Math.random() * choicesList.length);
+	let randomItem = choicesList[randomIndex];
+	return randomItem
+}
+
+// Test getComputerChoice
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+
